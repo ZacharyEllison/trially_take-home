8cb9e652fd993bb384eb774c5532939023c9bd85
Zachary Ellison <zach.r.ellison@gmail.com>
2024-07-19 12:06:26 -0400

    first step towards containerization

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,13 +1,12 @@
-from .settings import DB_USER, DB_PASS
+from .settings import DB_URL
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DB_URL = f"postgresql://{DB_USER}:{DB_PASS}@db/taskdb"
-
 engine = create_engine(DB_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+# this is for our ORM / CRUD
 Base = declarative_base()
 
 def get_db():
--- backend/app/settings.py
+++ backend/app/settings.py
@@ -1,6 +1,3 @@
 import os
 
-# there are other ways you can structure this as the base increases in complexity
-DB_USER = os.environ.get("DB_USER")
-DB_PASS = os.environ.get("DB_PASS")
-DB_PORT = os.environ.get("DB_PORT")
+DB_URL = os.environ.get("DB_URL")
