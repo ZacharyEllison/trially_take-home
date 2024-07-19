80642eba8c3ff477dba9bc029fb80074d3802a98
Zachary Ellison <zach.r.ellison@gmail.com>
2024-07-19 12:01:34 -0400

    backend db setup

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -1,3 +1,6 @@
 import os
 
+# there are other ways you can structure this as the base increases in complexity
 DB_USER = os.environ.get("DB_USER")
+DB_PASS = os.environ.get("DB_PASS")
+DB_PORT = os.environ.get("DB_PORT")
